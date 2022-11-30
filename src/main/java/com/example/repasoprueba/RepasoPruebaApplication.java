4664150bbb0eefeae0c6ca7da46356b7d924479c
Bairon Suarez <baironsuarez1205@gmail.com>
2022-11-29 22:06:13 -0500

    Creacion del proyecto

---
+++ src/main/java/com/example/repasoprueba/RepasoPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.repasoprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasoPruebaApplication.class, args);
+	}
+
+}
